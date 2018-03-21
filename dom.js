d2f4394f84c38ba8efc966487d9149ce816ec883
ishak <ishak-3@hotmail.com>
2018-03-21 11:53:47 +0200

    ishak edits

--- dom.js
+++ dom.js
@@ -5,20 +5,20 @@
     // This is the dom node where we will keep our todo
     var container = document.getElementById('todo-container');
     var addTodoForm = document.getElementById('add-todo');
-  
+
     var state = [
       { id: -3, description: 'first todo' },
       { id: -2, description: 'second todo' },
       { id: -1, description: 'third todo' },
     ]; // this is our initial todoList
-  
+
     // This function takes a todo, it returns the DOM node representing that todo
     var createTodoNode = function(todo) {
       var todoNode = document.createElement('li');
       // you will need to use addEventListener
-  
+
       // add span holding description
-  
+
       // this adds the delete button
       var deleteButtonNode = document.createElement('button');
       deleteButtonNode.addEventListener('click', function(event) {
@@ -26,46 +26,65 @@
         update(newState);
       });
       todoNode.appendChild(deleteButtonNode);
-  
+
       // add markTodo button
-  
+      var markButtonNode = document.createElement('input').type = "checkbox";
+       markButtonNode.addEventListener('click', function(event) {
+             var newState = todoFunctions.deleteTodo(state, todo.id);
+               update(newState);
+            });
+       
       // add classes for css
-  
+      markButtonNode.className = "mark";
+
       return todoNode;
     };
-  
+
+
     // bind create todo form
     if (addTodoForm) {
       addTodoForm.addEventListener('submit', function(event) {
         // https://developer.mozilla.org/en-US/docs/Web/Events/submit
         // what does event.preventDefault do?
         // what is inside event.target?
-  
+
         var description = '?'; // event.target ....
-  
+
         // hint: todoFunctions.addTodo
         var newState = []; // ?? change this!
         update(newState);
       });
     }
-  
+
     // you should not need to change this function
     var update = function(newState) {
       state = newState;
       renderState(state);
     };
-  
+//// ishak
     // you do not need to change this function
     var renderState = function(state) {
-      var todoListNode = document.createElement('ul');
-  
+      var divTodo = document.getElementById('todo');
+      var divDone = document.getElementById('done');
+      var todoListNode1 = document.createElement('ul');
+      var todoListNode2 = document.createElement('ul');
+
       state.forEach(function(todo) {
-        todoListNode.appendChild(createTodoNode(todo));
+        if (! todo.done) {
+          todoListNode1.appendChild(createTodoNode(todo));
+        }
+        else {
+          todoListNode2.appendChild(createTodoNode(todo));
+
+        }
       });
-  
+      divTodo.appendChild(todoListNode1)
+      divTodo.appendChild(todoListNode2)
+/// ishak
       // you may want to add a class for css
       container.replaceChild(todoListNode, container.firstChild);
     };
-  
-    if (container) renderState(state);
-  })();
+
+  if (container) renderState(state);
+
+})();
--- test.js
+++ test.js
@@ -1,7 +1,60 @@
 var test = require('tape');
 var logic = require('./logic');
+/// ishak test
+test("markTodo function", function(t) {
+    var arr = [
+        { id: 0, description: 'first todo', done: false},
+        { id: 1, description: 'second todo', done: false},
+        { id: 2, description: 'third todo', done: false}
+    ];
+    var actual=logic.markTodo(arr,1)
+    var expected = [
+        { id: 0, description: 'first todo', done: false},
+        { id: 1, description: 'second todo', done: true},
+        { id: 2, description: 'third todo', done: false}
+    ];
 
-test('Example test', function(t) {
-  t.pass();
-  t.end();
-});
+    t.deepEqual(actual, expected, 'second marked');
+    t.end();
+
+
+})
+
+test("markTodo function", function(t) {
+    var arr = [
+        { id: 0, description: 'first todo', done: false},
+        { id: 1, description: 'second todo', done: false},
+        { id: 2, description: 'third todo', done: false}
+    ];
+    var actual=logic.markTodo(arr,0)
+    var expected = [
+        { id: 0, description: 'first todo', done: true},
+        { id: 1, description: 'second todo', done: false},
+        { id: 2, description: 'third todo', done: false}
+    ];
+
+    t.deepEqual(actual, expected, 'second marked');
+    t.end();
+
+
+})
+
+test("markTodo function", function(t) {
+    var arr = [
+        { id: 0, description: 'first todo', done: false},
+        { id: 1, description: 'second todo', done: false},
+        { id: 2, description: 'third todo', done: false}
+    ];
+    var actual=logic.markTodo(arr,2)
+    var expected = [
+        { id: 0, description: 'first todo', done: false},
+        { id: 1, description: 'second todo', done: false},
+        { id: 2, description: 'third todo', done: true}
+    ];
+
+    t.deepEqual(actual, expected, 'second marked');
+    t.end();
+
+
+})
+///// ishak test
