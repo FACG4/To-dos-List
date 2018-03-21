65b31374385f9c252abb47c73eab5b5ab28fb690
haneenshahwan4 <haneenshahwan@hotmail.com>
2018-03-21 09:44:54 +0200

    add function

--- test.js
+++ test.js
@@ -1,14 +1,36 @@
 var test = require('tape');
 var logic = require('./logic');
 
-test('Example test', function(t) {
-  t.pass();
-  t.end();
-});
+// test('Example test', function(t) {
+//   t.pass();
+//   t.end();
+// });
 
 /* add tests*/
 
+var todos=[];
 
+test('Add task', function(t) {
+  const acual = logic.addTodo(todos,'sleep');
+  const exp =[{id:1,description:'sleep', done:'false'}];
+  t.deepEqual(acual,exp,'should return sleep');
+  t.end();
+
+});
+test('Add task', function(t) {
+  const acual = logic.addTodo(todos,'sleep');
+  const exp =[{id:2,description:'sleep', done:'false'}];
+  t.deepEqual(acual,exp,'should return sleep');
+  t.end();
+
+});
+test('Add task', function(t) {
+  const acual = logic.addTodo(todos,'sleep');
+  const exp =[{id:3,description:'sleep', done:'false'}];
+  t.deepEqual(acual,exp,'should return sleep');
+  t.end();
+
+});
 
 
 /* delete tests*/
@@ -25,5 +47,3 @@ test('Example test', function(t) {
 
 
 /* sortTodos tests */
-
-
